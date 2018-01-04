f97954191356de4c8b8f8126a7ca278e221f8934
Siva <sivaramaprasad11@gmail.com>
2018-01-04 16:35:56 +0530

    my first ccommit

---
+++ src/test/java/maven/AppTest.java
@@ -0,0 +1,19 @@
+package maven;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    
+    public AppTest( )
+    {
+     System.out.println("in test fie");
+    }
+
+}
